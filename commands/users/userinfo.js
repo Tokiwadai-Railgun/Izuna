21fbb07be355869dc6be283c493844a86bf8a826
Tokiwadai-Railgun <arthur.scheidbach@live.fr>
2023-03-18 09:22:16 +0100

    Dernier patch de la commande userInfo

--- commands/users/userinfo.js
+++ commands/users/userinfo.js
@@ -10,17 +10,21 @@ module.exports = {
         const member = await interaction.guild.members.fetch(interaction.targetId);
 
         const embed = new EmbedBuilder()
-            .setAuthor({ name: `${member.user.tag} (${member.user.id})`, iconURL: member.user == Izuna.user? 'https://static.wikia.nocookie.net/worldtrigger/images/7/71/Replica_infobox.png/revision/latest/scale-to-width-down/150?cb=20140725024605': "" })
+            .setAuthor({ name: `${member.user.tag} (${member.user.id})`, iconURL: member.user == Izuna.user? 'https://static.wikia.nocookie.net/worldtrigger/images/7/71/Replica_infobox.png/revision/latest/scale-to-width-down/150?cb=20140725024605': member.user.displayAvatarURL() })
             .setColor("#8e48f7")
             .setImage(member.user.displayAvatarURL())
-            .setThumbnail()
             .addFields([
-                { name: "Nom", value: `${member.displayName}, Online: true` },
+                { name: "Nom", value: `${member.displayName}` },
                 { name: "Modérateur", value: `${member.kickable ? "🔴" : "🟢"}`, inline: true },
-                { name: "Bot", value:`${member.user.bot? '🟢' : '🔴'}`, inline: true },
                 { name: "Rôles", value: `${member.roles.cache.map(roles => roles).join(", ")}`, inline: true },
             ])
         
+
+        if (member.user.bot) {
+            embed.addFields([
+                { name: "Bot", value:`🟢`, inline: false },
+            ])
+        }
         interaction.reply({ embeds: [embed], ephemeral: true });
     }
 }
